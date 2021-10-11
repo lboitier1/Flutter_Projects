1ca7ec0172c99c11c0a85e4b314f96efe6163737
Lucas Boitier <lucasboitier@MacBook-Pro-de-Lucas-2.local>
2021-10-11 21:13:09 +0200

    initial commit

---
+++ portfolio/lib/utilities/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kLightYellow     =   Color(0xFFFCF4C4); //#FCF4C4
+
+const kLightGreen      =   Color(0xFFC6DBBE); //#C6DBBE
+const kDarkGreen       =   Color(0xFF91AA9D); //#91AA9D
+
+const kLightBlue       =   Color(0xFFD9E8F5); //#D9E8F5
+const kMediumBlue      =   Color(0xFF3E606F); //#3E606F
+const kDarkBlue        =   Color(0xFF193441); //#193441
+
+
