9aebe9096b8e8a33a39c5e9b6cc6e988ffee5a35
lboitier1 <lboitier1@gmail.com>
2022-01-26 00:08:28 +0100

    finish answerBall app

--- answerall/lib/main.dart
+++ answerall/lib/main.dart
@@ -1,15 +1,56 @@
+import 'dart:developer';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[900],
+        title: Text("Ask Me Anything"),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            // int ballNumber = Random().nextInt(6) + 1;
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
   }
 }
