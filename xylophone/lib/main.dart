be7bd862e8c2f522edcee4a41f890666118b646a
lboitier1 <lboitier1@gmail.com>
2022-02-14 21:48:10 +0100

    make all the sounds appear and start xylophone design

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,28 +4,79 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[400],
         body: SafeArea(
-          child: Container(
-            child: Center(
-              child: TextButton(
-                child: Text('Tap to play'),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play("note1.wav");
-                },
+          child: Column(children: [
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
               ),
+              onPressed: () {
+                playSound(1);
+              },
             ),
-          ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orange),
+              ),
+              onPressed: () {
+                playSound(2);
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.yellow),
+              ),
+              onPressed: () {
+                playSound(3);
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              ),
+              onPressed: () {
+                playSound(4);
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightGreen),
+              ),
+              onPressed: () {
+                playSound(5);
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {
+                playSound(6);
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlue),
+              ),
+              onPressed: () {
+                playSound(7);
+              },
+            ),
+          ]),
         ),
       ),
     );
